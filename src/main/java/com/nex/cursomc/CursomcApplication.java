9d7ab6876ee7de82a2fab702d3bfe67210ed0773
Robson Rosa <mecaunip2015@gmail.com>
2018-09-26 22:49:31 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/nex/cursomc/CursomcApplication.java
+++ src/main/java/com/nex/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.nex.cursomc.domain.Cidade;
 import com.nex.cursomc.domain.Cliente;
 import com.nex.cursomc.domain.Endereco;
 import com.nex.cursomc.domain.Estado;
+import com.nex.cursomc.domain.ItemPedido;
 import com.nex.cursomc.domain.Pagamento;
 import com.nex.cursomc.domain.PagamentoComBoleto;
 import com.nex.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.nex.cursomc.repositories.CidadeRepository;
 import com.nex.cursomc.repositories.ClienteRepository;
 import com.nex.cursomc.repositories.EnderecoRepository;
 import com.nex.cursomc.repositories.EstadoRepository;
+import com.nex.cursomc.repositories.ItemPedidoRepository;
 import com.nex.cursomc.repositories.PagamentoRepository;
 import com.nex.cursomc.repositories.PedidoRepository;
 import com.nex.cursomc.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class CursomcApplication implements CommandLineRunner{
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -113,6 +117,19 @@ public class CursomcApplication implements CommandLineRunner{
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));;
 		
-	}
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
+		
+		}
 }
 
--- src/main/java/com/nex/cursomc/domain/Pedido.java
+++ src/main/java/com/nex/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.nex.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +36,9 @@ public class Pedido implements Serializable{
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
+	
+	@OneToMany(mappedBy="id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Pedido() {
 		
@@ -109,5 +115,13 @@ public class Pedido implements Serializable{
 		this.instante = instante;
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 }
