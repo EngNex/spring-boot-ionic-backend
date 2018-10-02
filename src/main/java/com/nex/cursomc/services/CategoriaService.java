08a0ecd9d8d1ed411bb7387d84503bb647af2031
Robson Rosa <mecaunip2015@gmail.com>
2018-10-01 23:30:24 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/nex/cursomc/services/CategoriaService.java
+++ src/main/java/com/nex/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.nex.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,9 @@ public class CategoriaService {
 		catch(DataIntegrityViolationException e) {
 		throw new DataIntegrityException("Não é possivel excluir uma categoria com produtos cadastrados");
 		}
+	}
+	public List<Categoria> findAll(){
+		return repo.findAll();		
 	}
 }
 
