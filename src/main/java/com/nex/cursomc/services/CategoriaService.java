cf4b9d21ea14dc0568e6a7637f72442a08550e5e
Robson Rosa <mecaunip2015@gmail.com>
2018-10-01 23:14:31 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/nex/cursomc/services/CategoriaService.java
+++ src/main/java/com/nex/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.nex.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.nex.cursomc.domain.Categoria;
 import com.nex.cursomc.repositories.CategoriaRepository;
+import com.nex.cursomc.services.exception.DataIntegrityException;
 import com.nex.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer  id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+		throw new DataIntegrityException("Não é possivel excluir uma categoria com produtos cadastrados");
+		}
 	}
 }
 
