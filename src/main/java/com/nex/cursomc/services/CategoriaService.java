d3cec7d94b0637ca19ed5a00da00681c60858e4e
Robson Rosa <mecaunip2015@gmail.com>
2018-10-02 00:00:46 -0300

    Paginação com parametros adicionais na paginação

--- src/main/java/com/nex/cursomc/services/CategoriaService.java
+++ src/main/java/com/nex/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nex.cursomc.domain.Categoria;
@@ -45,6 +48,12 @@ public class CategoriaService {
 	}
 	public List<Categoria> findAll(){
 		return repo.findAll();		
+	}
+	public Page<Categoria> findPage(Integer page,Integer linesPerPage,String ordeBy, String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), ordeBy);
+		return repo.findAll(pageRequest);
+		
 	}
 }
 
