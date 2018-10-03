da347e33d161b13ec50385f38ba56ba80dda3340
Robson Rosa <mecaunip2015@gmail.com>
2018-10-02 22:34:10 -0300

    Validação sintatica vom Bean Validation

--- src/main/java/com/nex/cursomc/services/CategoriaService.java
+++ src/main/java/com/nex/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nex.cursomc.domain.Categoria;
+import com.nex.cursomc.dto.CategoriaDTO;
 import com.nex.cursomc.repositories.CategoriaRepository;
 import com.nex.cursomc.services.exception.DataIntegrityException;
 import com.nex.cursomc.services.exception.ObjectNotFoundException;
@@ -54,6 +55,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), ordeBy);
 		return repo.findAll(pageRequest);
 		
+	}
+	public Categoria fromDTO(CategoriaDTO objDto ) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
 
